a8bb175bff223957145b28cf1496bad4f0be7c41
Zach Shea <zacharyshea@icloud.com>
2019-01-26 23:43:50 -0500

    Add responsiveness to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Size from '../Size.js'
 import dribbble from '../assets/icons/social-dribbble.svg';
 import instagram from '../assets/icons/social-instagram.svg';
 import twitter from '../assets/icons/social-twitter.svg';
@@ -7,50 +8,86 @@ import linkedin from '../assets/icons/social-linkedin.svg';
 import soundcloud from '../assets/icons/social-soundcloud.svg';
 import youtube from '../assets/icons/social-youtube.svg';
 
-const Container = {
+const Main = {
+  width: "100%"
+}
+
+const ContainerDesktop = {
   maxWidth: "1080px",
   marginLeft: "auto",
   marginRight: "auto",
-  padding: "130px 100px 70px 100px"
+  padding: "90px 100px 70px 100px"
+}
+
+const ContainerDefault = {
+  maxWidth: "1080px",
+  marginLeft: "auto",
+  marginRight: "auto",
+  padding: "40px 30px 50px 30px"
 }
 
 const Socials = {
     marginBottom: "60px",
     display: "flex",
+    flexWrap: "wrap",
     alignItems: "center"
 }
 
-const SocialIcon = {
-    marginRight: "50px"
+const SocialIconDesktop = {
+    marginRight: "50px",
+    marginTop: "40px"
 }
 
-const Copyright = {
+const SocialIconDefault = {
+    marginRight: "40px",
+    marginTop: "40px"
+}
+
+const CopyrightDesktop = {
   fontSize: "20px",
   fontWeight: "500",
   letterSpacing: "1px",
   color: "rgba(255,255,255,0.6)"
 }
 
+const CopyrightDefault = {
+  fontSize: "15px",
+  fontWeight: "500",
+  color: "rgba(255,255,255,0.6)"
+}
+
 class Footer extends React.Component {
     render() {
-      const Main = {
-        width: "100%"
-      }
-
       return (
         <div style={Main}>
-            <div style={Container}>
+          <Size.Desktop>
+            <div style={ContainerDesktop}>
                 <div style={Socials}>
-                    <a style={SocialIcon} href="https://dribbble.com/ZachShea" target="_blank"><img src={dribbble}/></a>
-                    <a style={SocialIcon} href="https://www.instagram.com/shea_zach/" target="_blank"><img src={instagram}/></a>
-                    <a style={SocialIcon} href="https://twitter.com/ZachShea" target="_blank"><img src={twitter}/></a>
-                    <a style={SocialIcon} href="https://github.com/ZachShea" target="_blank"><img src={github}/></a>
-                    <a style={SocialIcon} href="https://www.linkedin.com/in/ZachShea" target="_blank"><img src={linkedin}/></a>
-                    <a style={SocialIcon} href="https://soundcloud.com/zach-shea" target="_blank"><img src={soundcloud}/></a>
-                    <a style={SocialIcon} href="https://www.youtube.com/channel/UCn9JCFB_Awk-z9suVHT8cWg" target="_blank"><img src={youtube}/></a>
+                    <a style={SocialIconDesktop} href="https://dribbble.com/ZachShea" target="_blank"><img src={dribbble}/></a>
+                    <a style={SocialIconDesktop} href="https://www.instagram.com/shea_zach/" target="_blank"><img src={instagram}/></a>
+                    <a style={SocialIconDesktop} href="https://twitter.com/ZachShea" target="_blank"><img src={twitter}/></a>
+                    <a style={SocialIconDesktop} href="https://github.com/ZachShea" target="_blank"><img src={github}/></a>
+                    <a style={SocialIconDesktop} href="https://www.linkedin.com/in/ZachShea" target="_blank"><img src={linkedin}/></a>
+                    <a style={SocialIconDesktop} href="https://soundcloud.com/zach-shea" target="_blank"><img src={soundcloud}/></a>
+                    <a style={SocialIconDesktop} href="https://www.youtube.com/channel/UCn9JCFB_Awk-z9suVHT8cWg" target="_blank"><img src={youtube}/></a>
                 </div>
-                <div style={Copyright}>© 2019 Zachary Shea.  All rights reserved.</div>
+                <div style={CopyrightDesktop}>© 2019 Zachary Shea.  All rights reserved.</div>
             </div>
+          </Size.Desktop>
+          <Size.Default>
+            <div style={ContainerDefault}>
+                <div style={Socials}>
+                    <a style={SocialIconDefault} href="https://dribbble.com/ZachShea" target="_blank"><img src={dribbble}/></a>
+                    <a style={SocialIconDefault} href="https://www.instagram.com/shea_zach/" target="_blank"><img src={instagram}/></a>
+                    <a style={SocialIconDefault} href="https://twitter.com/ZachShea" target="_blank"><img src={twitter}/></a>
+                    <a style={SocialIconDefault} href="https://github.com/ZachShea" target="_blank"><img src={github}/></a>
+                    <a style={SocialIconDefault} href="https://www.linkedin.com/in/ZachShea" target="_blank"><img src={linkedin}/></a>
+                    <a style={SocialIconDefault} href="https://soundcloud.com/zach-shea" target="_blank"><img src={soundcloud}/></a>
+                    <a style={SocialIconDefault} href="https://www.youtube.com/channel/UCn9JCFB_Awk-z9suVHT8cWg" target="_blank"><img src={youtube}/></a>
+                </div>
+                <div style={CopyrightDefault}>© 2019 Zachary Shea.  All rights reserved.</div>
+            </div>
+          </Size.Default>
         </div>
       );
     }
